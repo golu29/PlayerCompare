0508f26080497613d2062ca74924f8ffbbd80109
golu29 <golu.sidd@gmail.com>
2017-11-14 08:16:22 +0530

    players stats(buggy code)

--- my_code.R
+++ my_code.R
@@ -1,1 +1,9 @@
 #Copare Players Stats
+Name <- c("Amla", "Kohli", "AB de Villiers")
+age <- c(34, 29, 32)
+matches <- c(158, 202, 225)
+cf <- c("26/34", "32/45", "25/53")
+# data frame for players record
+stats_df <- data.frame(Name, age, matches, cf)
+stats_df
+
