c6949c513301b20ab1cba8ba5db4cdda7fa9b8bd
golu29 <golu.sidd@gmail.com>
2017-11-14 15:58:49 +0530

    bug fixed

--- my_code.R
+++ my_code.R
@@ -4,6 +4,6 @@ age <- c(34, 29, 32)
 matches <- c(158, 202, 225)
 c_f <- c("26/34", "32/45", "25/53")
 # data frame for players record
-records_df <- data.frame(Name, age, matches, cf)
-print(records_df)
+records_df <- data.frame(Name, age, matches, c_f)
+records_df
 
